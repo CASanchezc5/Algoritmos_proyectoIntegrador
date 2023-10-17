77fc38b430c924f139be10b8b2f583dc4bb5b464
Carlos Andrés <144819672+CASanchezc5@users.noreply.github.com>
2023-10-16 19:55:58 -0500

    cambio algoritmo

--- Algoritmo7.java
+++ Algoritmo7.java
@@ -1,17 +1,37 @@
+//CALCULAR EL ÁREA O PERIMETRO DE UN ROMBO
+
 import java.util.Scanner;
 
 public class Algoritmo7 {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Ingresa el valor de la diagonal mayor: ");
-        double diagonalMayor = scanner.nextDouble();
+        Scanner input = new Scanner(System.in);
         
-        System.out.print("Ingresa el valor de la diagonal menor: ");
-        double diagonalMenor = scanner.nextDouble();
+        System.out.println("Elije área o perimetro del rombo");
+        System.out.println("Area");
+        System.out.println("Perimetro");
         
-        double perimetro = 4 * Math.sqrt((diagonalMayor * diagonalMayor + diagonalMenor * diagonalMenor) / 4);
+        String opcion = input.nextLine();
         
-        System.out.println("El perímetro del rombo es: " + perimetro);
+        
+        if("Perimetro".equals(opcion)){
+            System.out.println("Ingrese un lado");
+            double lado = input.nextDouble();
+            double perimetro = 4 * lado;
+            System.out.println("El perimetro del rombo es: "+perimetro);
+        }else if("Area".equals(opcion)){
+            System.out.println("Ingrese la Diagonal Mayor");
+            double diagMayor = input.nextDouble();
+            System.out.println("Ingrese la Diagonal Menor");
+            double diagMenor = input.nextDouble();
+            double area = (diagMayor * diagMenor)/2;
+            System.out.println("El área del rombo es: "+area);
+            
+        }else{
+            System.out.println("Opción no valida");
+        }
+               
     }
+    
 }
