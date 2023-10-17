6bcb6e925604d586dd3976b42739e2ef858fa204
Carlos Andrés <144819672+CASanchezc5@users.noreply.github.com>
2023-10-17 09:42:33 -0500

    cambio en clase nombre

--- Algoritmo2.java
+++ Algoritmo2.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 import java.lang.Math;
-class Main {
+public class Algoritmo2 {
 public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
 
